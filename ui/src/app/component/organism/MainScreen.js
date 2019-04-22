48d1f5631998e4a77fd946b56b780ca276551fb7
Hendrik Ishikawa <tatsuya.iskw@gmail.com>
2019-04-22 19:34:50 +0200

    Add basic layout for main list screen

--- ui/src/app/component/organism/MainScreen.js
+++ ui/src/app/component/organism/MainScreen.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchInitialData, blockReport, resolveReport } from '../../action';
 
 const mapStateToProps = state => {
     return {
+        reports: state.reports
     };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
+    fetchInitialData: () => dispatch(fetchInitialData()),
+    blockReport: (id) => dispatch(blockReport(id)),
+    resolveReport: (id) => dispatch(resolveReport(id))
 });
 
 class Main extends Component {
@@ -20,8 +25,41 @@ class Main extends Component {
 
     render() {
         return (
-            <div className="MainScreen">
-                <h2>MainScreen</h2>
+            <div className="reports">
+                <h2>REPORTS (OPEN)</h2>
+                <ul>
+                    {
+                        this.props.reports.map((report) => {
+                            return (
+                                <div className="reports_box" style={{ display: "flex", height: 80 }}>
+                                    <div>
+                                        <div>id: {report['id']}</div>
+                                        <div>state: {report['state']}</div>
+                                        <div>Details</div>
+                                    </div>
+                                    <div>
+                                        <div>Type: Spam</div>
+                                        <div>Message: Some Messages ...</div>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <button onClick={() => {
+                                                this.props.blockReport(report['id'])
+                                                this.forceUpdate()
+                                            }}>Block</button>
+                                        </div>
+                                        <div>
+                                            <button onClick={() => {
+                                                this.props.resolveReport(report['id'])
+                                                this.forceUpdate()
+                                            }}>Resolve</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </ul>
             </div>
         );
     }
