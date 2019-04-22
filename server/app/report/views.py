766d41df843c5fd354fa40833dbaef4565f3e370
Hendrik Ishikawa <tatsuya.iskw@gmail.com>
2019-04-22 19:05:23 +0200

    Implement basic logics to make http response to request as REST API

--- server/app/report/views.py
+++ server/app/report/views.py
@@ -1,3 +1,45 @@
+import json
 from django.shortcuts import render
+from django.http import QueryDict, JsonResponse, HttpResponse
+from django.views.generic import View
+from django.views.decorators.csrf import csrf_exempt
+from .models import Report
 
-# Create your views here.
+class FetchList(View):
+    def get(self, request, *args, **kwargs):
+        all_reports = Report.objects.all()
+        json_to_response = {"reports": []}
+
+        for report in all_reports:
+            json_to_response["reports"].append({
+            "id": report.reference_id,
+            "source": "REPORT",
+            "state": report.status,
+            "payload": {
+                "source": "REPORT",
+                "reportType": "SPAM",
+                "message": "",
+                "reportId": "",
+            },
+            "created": "2017-10-02T16:09:04.258Z"
+        })
+
+        return JsonResponse(json_to_response, safe=False)
+
+@csrf_exempt
+def report_handler(request, reference_id=""):
+    if request.method == "PUT":
+        print (reference_id)
+
+        body = json.loads(request.body)
+        status = body['target_status']
+
+        r = Report.objects.get(reference_id=reference_id)
+
+        if status == "BLOCK":
+            r.status = "BLOCK"
+        elif status == "RESOLVE":
+            r.status = "RESOLVE"
+        r.save()
+
+        return JsonResponse({"reference_id": r.status}, safe=False)
