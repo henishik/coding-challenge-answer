858ae880f2d5bb5436494b2101e55939ff89f8ff
Hendrik Ishikawa <tatsuya.iskw@gmail.com>
2019-04-22 19:19:59 +0200

    Embed a logic to populate initial data based on provided data structure

--- server/docker-scripts/start.sh
+++ server/docker-scripts/start.sh
@@ -2,5 +2,7 @@
 
 # Run django migrate command before app runs.
 /docker-scripts/django-migrate.sh
+# Initialize data.
+/docker-scripts/initialize-data.sh
 # Run processes through supervisor.
 exec supervisord -n
