62b9cc310054ca1174ae22ade45f3ba37a533235
Hendrik Ishikawa <tatsuya.iskw@gmail.com>
2019-04-28 20:54:28 +0200

    General clean-up by deleting unnecessary comments

--- server/app/project/settings.py
+++ server/app/project/settings.py
@@ -34,21 +34,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 ROOT_URLCONF = 'project.urls'
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
 WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
@@ -56,21 +41,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#     },
-# ]
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
--- server/docker-scripts/start.sh
+++ server/docker-scripts/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Run django migrate command before app runs.
 /docker-scripts/django-migrate.sh
-# Initialize data.
 /docker-scripts/initialize-data.sh
-# Run processes through supervisor.
 exec supervisord -n
