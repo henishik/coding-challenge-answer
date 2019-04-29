d8f93b95a98ab62fdb7ca14bac057acf13c9bd36
Hendrik Ishikawa <tatsuya.iskw@gmail.com>
2019-04-29 15:45:50 +0200

    decouple component scss definitions from global scss file

--- ui/src/app/component/molecule/ReportListCell/ReportListCell.js
+++ ui/src/app/component/molecule/ReportListCell/ReportListCell.js
@@ -1,33 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import './style.scss';
+import './style.scss';
 
 const ReportListCell = ({ report, onUpdateReportState}) => {
     return (
-        <div key={report['reference_id']} className="reports__box" style={{ display: "flex", height: 80 }}>
-            <div className="reports__box__item__large">
-                <div className="reports__box__row">id: {report['reference_id']}</div>
-                <div className="reports__box__row">
-                    state: <span className={"reports__box__status__" + report['status']}>
+        <div key={report['reference_id']} className="reportListCell" style={{ display: "flex", height: 80 }}>
+            <div className="reportListCell__item__large">
+                <div className="reportListCell__row">id: {report['reference_id']}</div>
+                <div className="reportListCell__row">
+                    state: <span className={"reportListCell__status__" + report['status']}>
                         {report['status']}
                     </span>
                 </div>
-                <div className="reports__box__row"><a href="#" onClick={() => console.log("routing function")}>Details</a></div>
+                <div className="reportListCell__row">
+                    <a href="#" onClick={() => console.log("routing function")}>
+                        Details
+                    </a>
+                </div>
             </div>
-            <div className="reports__box__item__large">
-                <div className="reports__box__row">Type: {report['report_type']}</div>
-                <div className="reports__box__row">Message: {report['message']}</div>
+
+            <div className="reportListCell__item__large">
+                <div className="reportListCell__row">Type: {report['report_type']}</div>
+                <div className="reportListCell__row">Message: {report['message']}</div>
             </div>
-            <div className="reports__box__item">
-                <div className="reports__box__row">
-                    <button className="reports__box__item__button" onClick={() => {
+
+            <div className="reportListCell__item">
+                <div className="reportListCell__row">
+                    <button className="reportListCell__item__button" onClick={() => {
                         onUpdateReportState(report['reference_id'], {
                             ticketState: "BLOCK"
                         })
                     }}>Block</button>
                 </div>
-                <div className="reports__box__row">
-                    <button className="reports__box__item__button" onClick={() => {
+                <div className="reportListCell__row">
+                    <button className="reportListCell__item__button" onClick={() => {
                         onUpdateReportState(report['reference_id'], {
                             ticketState: "CLOSED"
                         })
