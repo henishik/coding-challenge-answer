00eb338d74340f1674a0cf66bc9881ae45acd8bd
Hendrik Ishikawa <tatsuya.iskw@gmail.com>
2019-04-29 09:13:42 +0200

    clean up basic unit tests for each api calls in server app

--- server/app/report/views.py
+++ server/app/report/views.py
@@ -9,28 +9,43 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ReportSerializer
 
+HTTP_METHOD_GET = "GET"
+HTTP_METHOD_PUT = "PUT"
+
 @csrf_exempt
 def report_handler(request, reference_id=""):
-    if request.method == "PUT":
+    if request.method == HTTP_METHOD_PUT:
         body = json.loads(request.body)
-        status = body['target_status']
 
-        r = Report.objects.get(reference_id=reference_id)
+        if ('target_status' in body):
+            state = body['target_status']
+            try:
+                r = Report.objects.get(reference_id=reference_id)
+            except Report.DoesNotExist:
+                return JsonResponse({}, status=status.HTTP_404_NOT_FOUND)
 
-        if status == "BLOCK":
-            r.status = "BLOCK"
-        elif status == "RESOLVE":
-            r.status = "RESOLVE"
-        r.save()
+            if state == "BLOCK":
+                r.status = "BLOCK"
+            elif state == "RESOLVE":
+                r.status = "RESOLVE"
+            r.save()
 
-        return JsonResponse({"reference_id": r.status}, safe=False)
+            return JsonResponse({"reference_id": r.status}, safe=False)
+        else:
+            return JsonResponse({}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return JsonResponse({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-@api_view(['GET', 'POST'])
+@api_view([HTTP_METHOD_GET])
 def get_post_reports(request):
-    if request.method == 'GET':
-        reports = Report.objects.all()
+    if request.method == HTTP_METHOD_GET:
+        try:
+            reports = Report.objects.all()
+        except Report.DoesNotExist:
+            Response(status=status.HTTP_404_NOT_FOUND)
+
         serializer = ReportSerializer(reports, many=True)
 
         return Response(serializer.data)
-    elif request.method == 'POST':
-        return Response({})
+    else:
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
