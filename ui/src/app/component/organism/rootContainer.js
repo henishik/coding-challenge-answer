d32ff4150f1cae83be7a115fafe835543193bb21
Hendrik Ishikawa <tatsuya.iskw@gmail.com>
2019-04-29 11:13:48 +0200

    Add basic logics of unit-tests for reducer in ui app

--- ui/src/app/component/organism/rootContainer.js
+++ ui/src/app/component/organism/rootContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router';
 
 const mapStateToProps = (state, ownProps) => ({});
 const mapDispatchToProps = (dispatch, ownProps) => ({});
---
+++ ui/src/app/test/reducer.test.js
@@ -0,0 +1,53 @@
+import reducer from '../reducers/index';
+import { RECIEVE_INITIAL_DATA, BLOCK_REPORT, RESOLVE_REPORT } from '../action/index';
+
+const givenState = {
+    reports: [],
+    routing: { "locationBeforeTransitions": null }
+};
+
+describe('A Test Suite on report reducer', () => {
+    it('Given emply action state: should have initial state', () => {
+        expect(reducer(givenState, {})).toEqual(givenState);
+    });
+
+    it('Given not recognized action type: should not affect any state change', () => {
+        expect(reducer(givenState, { type: 'NOT_EXISTING' })).toEqual(givenState);
+    });
+
+    it('FETCH_ALL_REPORT: should affect state change with response data', () => {
+        let responseDataMock = [{
+            "reference_id": "0103e005-b762-485f-8f7e-722019d4f302",
+            "status": "RESOLVE",
+            "message": "",
+            "source": "",
+            "report_type": "SPAM",
+            "created_at": "2019-04-28T12:44:58.740220Z",
+            "updated_at": "2019-04-28T12:44:58.740335Z"
+        }]
+
+        let expectedState = {
+            reports: responseDataMock,
+            routing: { "locationBeforeTransitions": null }
+        }
+
+        expect(reducer(
+            givenState,
+            { type: RECIEVE_INITIAL_DATA, data: responseDataMock })
+        ).toEqual(expectedState);
+    });
+
+    it('BLOCK_REPORT: should not affect any state change', () => {
+        expect(reducer(
+            givenState,
+            { type: BLOCK_REPORT })
+        ).toEqual(givenState);
+    });
+
+    it('RESOLVE_REPORT: should not affect any state change', () => {
+        expect(reducer(
+            givenState,
+            { type: RESOLVE_REPORT })
+        ).toEqual(givenState);
+    });
+});
