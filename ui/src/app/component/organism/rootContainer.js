1cb49deb00d92851c7c893ba4556ecf9d3d8ba41
hendrik (Tatsu) Ishikawa <hendrik.ishikawa@publicismedia.com>
2020-06-02 13:59:11 +0200

    create a separate branch for publicis sales app

--- ui/src/app/component/organism/rootContainer.js
+++ ui/src/app/component/organism/rootContainer.js
@@ -9,7 +9,16 @@ class rootContainer extends Component {
         return (
             <div className="rootContainer">
                 <div className="rootContainer__sidebar">
-                    <h1 className="rootContainer__sidebar__logo">Spam Report Dashboard</h1>
+                    <h1 className="rootContainer__sidebar__logo">Publicis People Cloud</h1>
+                    <h3>CX-Loop</h3>
+                    <h3>Growth Builder</h3>
+                    <h3>POE-ES</h3>
+                    <h3>ROI Optimizer</h3>
+                    <h3>ROI Tracker</h3>
+                    <h3>X-Cross</h3>
+                    <h3>Audience Builder</h3>
+                    <h3>Global Reporting</h3>
+                    <h3>KPI Reporting</h3>
                 </div>
                 <div className="rootContainer__main">
                     {this.props.children}
